c77bb62ee57be47751bba039b2cd1eedcff461c8
jakulej <j.kulej01@gmail.com>
2025-11-15 13:00:44 +0100

    Added rust analyzer

--- nvim/lua/lsp_servers.lua
+++ nvim/lua/lsp_servers.lua
@@ -1,1 +1,29 @@
+local lspconfig = require'lspconfig'
 
+local on_attach = function(client)
+    require'completion'.on_attach(client)
+end
+
+lspconfig.rust_analyzer.setup({
+    on_attach = function(client, bufnr)
+        vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+    end,
+    settings = {
+        ["rust-analyzer"] = {
+            imports = {
+                granularity = {
+                    group = "module",
+                },
+                prefix = "self",
+            },
+            cargo = {
+                buildScripts = {
+                    enable = true,
+                },
+            },
+            procMacro = {
+                enable = true
+            },
+        }
+    }
+})
